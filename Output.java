4b6ccbbf6618cb1b84d9b394888b3b1cd033ab43
keoneoliveros <oliveros.keian@gmail.com>
2019-09-17 09:52:56 +0800

    commit as queen andaya

---
+++ Output.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.util.Scanner;
+import java.io.File;
+
+
+public class Output {
+	
+		
+  public static void main(String[] args) throws IOException {
+    	
+ 	
+    System.out.println("File Name:  ");
+    Scanner scn = new Scanner(System.in);
+    
+    	File ffile = new File(scn.nextLine() + ".queen");
+    	scn = new Scanner(ffile);
+    	
+    	String snn = scn.nextLine();
+    	System.out.println(snn);
+    	
+    	scn.close();
+}
+}
